c3ab84a4dea224773a0278349c2b6b94d6d9b413
Balita-R <150338044+Balita-R@users.noreply.github.com>
2024-08-26 16:42:41 +0200

    Fixed the base for the frontend

---
+++ client/xu-inventory-website/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './layout';
+import Home from './home';
+
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+
+        {/* Add more routes here */}
+      </Route>
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
