437a40627dc74cc26411dc6b2e8e88b6cf2c7efd
Balita-R <150338044+Balita-R@users.noreply.github.com>
2024-08-27 18:39:13 +0200

    Added Inventory pageish

--- client/xu-inventory-website/src/App.js
+++ client/xu-inventory-website/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layout';
 import Home from './home';
+import Inventory from './inventory';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="inventory" element={<Inventory />} />
         </Route>
       </Routes>
     </Router>
