f0b407d3376c7a2036caf308720f68131444af0a
MarieBelle88 <137484339+MarieBelle88@users.noreply.github.com>
2024-08-27 22:14:46 +0200

    fixed header n sidebar

--- client/xu-inventory-website/src/layout.js
+++ client/xu-inventory-website/src/layout.js
@@ -6,13 +6,28 @@ function Layout() {
   return (
     <div className="layout">
       <div className="topBar">
-        <img
-          style={{ height: '200px' }}
-          src="https://cdn.schuelerkarriere.de/schuelerkarriere/2bfe712d-93ec-48af-9941-d4ea50dc3be9%252Fxu-exponential-university_logo.webp"
-          alt="XU Logo"
-        />
-        <h2>XU Inventory Management</h2>
-      </div>
+
+      <ul className="logo">
+<Link to="/">
+      <img
+        style={{
+          height: '50px',
+          width: '50px', /* Ensure width matches height for a perfect circle */
+          borderRadius: '50%', /* Make the image circular */
+          objectFit: 'cover' /* Optional: Ensures image covers the circle properly */
+        }}
+        src="https://cdn.schuelerkarriere.de/schuelerkarriere/2bfe712d-93ec-48af-9941-d4ea50dc3be9%252Fxu-exponential-university_logo.webp"
+        alt="XU Logo"
+      />
+    </Link>
+</ul>
+
+<ul className="centeredContent">
+  <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+    <h2>XU Inventory Management</h2>
+  </Link>
+</ul>
+    </div>
       
       <div className="contentWrapper">
         <nav className="sidebar">
