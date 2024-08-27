1ca634e19721a32ab6da7c58afc7e347419ee67b
Balita-R <150338044+Balita-R@users.noreply.github.com>
2024-08-27 18:52:17 +0200

    Connected pages to sidebar

--- client/xu-inventory-website/src/layout.js
+++ client/xu-inventory-website/src/layout.js
@@ -19,9 +19,9 @@ function Layout() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/inventory">Inventory </Link></li>
-            <li><Link to="/employees">Users</Link></li>
-            <li><Link to="/transactions">Add Item</Link></li>
-            <li><Link to="/transactions">Add User</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/usersForm">Add User</Link></li>
+            <li><Link to="/inventoryForm">Add Item</Link></li>
 
           </ul>
         </nav>
