8eb17dc5fc255d22c4f25deceb0573079985bd68
Balita-R <150338044+Balita-R@users.noreply.github.com>
2024-08-30 17:31:49 +0200

    Tried working out the inventory table but not working

--- client/xu-inventory-website/src/App.js
+++ client/xu-inventory-website/src/App.js
@@ -6,6 +6,7 @@ import Inventory from './inventory';
 import InventoryForm from './inventoryForm';
 import UsersForm from './usersForm';
 import Users from './users';
+import InventoryDetail from './inventorydetail';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="inventory" element={<Inventory />} />
+          <Route path="inventory/:id" element={<InventoryDetail />} />
           <Route path="users" element={<Users />} />
           <Route path="inventoryForm" element={<InventoryForm />} />
           <Route path="usersForm" element={<UsersForm />} />
--- client/xu-inventory-website/src/inventory.js
+++ client/xu-inventory-website/src/inventory.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import './inventory.css';
 import { useNavigate } from 'react-router-dom';
 import './inventory.css';
-
-
 
 function Inventory() {
   const [assets, setAssets] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch assets data from the API
-    fetch('/api/assets')
-      .then(response => response.json())
-      .then(data => setAssets(data))
+    // Fetch data from the backend
+    fetch('http://localhost:3001/assets')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Fetched assets:', data); // Log the data to verify
+        setAssets(data);
+      })
       .catch(error => console.error('Error fetching assets:', error));
   }, []);
 
@@ -38,83 +43,27 @@ function Inventory() {
           </tr>
         </thead>
         <tbody>
-          {assets.map(asset => (
-            <tr key={asset.id} onClick={() => handleRowClick(asset.id)}>
-              <td>{asset.id}</td>
-              <td>{asset.title}</td>
-              <td>{asset.asset_id}</td>
-              <td>{asset.company}</td>
-              <td>{asset.asset_type}</td>
-              <td>{asset.serial_number}</td>
-              <td>{asset.assigned_to}</td>
+          {assets.length > 0 ? (
+            assets.map(asset => (
+              <tr key={asset.id} onClick={() => handleRowClick(asset.id)}>
+                <td>{asset.id}</td>
+                <td>{asset.title}</td>
+                <td>{asset.asset_id}</td>
+                <td>{asset.company}</td>
+                <td>{asset.asset_type}</td>
+                <td>{asset.serial_number}</td>
+                <td>{asset.assigned_to}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="7">No assets available</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
   );
 }
 
-
-
-
-
-/*function Inventory() {
-    return (
-                 
-      <div>
-      <h1>Hi this is the inventory</h1>
-      
-      <div className="search-box">
-        <input type="text" placeholder="Search" />
-        <button><i className="fas fa-search"></i></button>
-      </div>
-  
-      <div className="dataTableContainer">
-      <table id="example" class="display" >
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011-04-25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011-07-25</td>
-                <td>$170,750</td>
-            </tr>
-            </tbody>
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-    </table>
-      </div>
-    </div>
-  );
-   
-  }*/
-
-  export default Inventory;
+export default Inventory;
